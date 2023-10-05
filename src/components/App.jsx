2075faa8776fec421edeb6686a4244dd28a4e844
Lewis Knapp <lknapp1888@gmail.com>
2023-10-05 18:16:49 +0100

    Add input form, state, and add to list function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import uniqid from 'uniqid';
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const [numList, setNumList] = useState({})
 
+  function addToList(e) {
+    e.preventDefault()
+    setNumList({
+      ...numList,
+      [uniqid()]: number
+    })
+    setNumber(0)
+  }
 
   return (
     <div className='app'>
+      <form onSubmit={(e) => addToList(e)}>
+        <input type="number" value={number} onChange={(e) => setNumber(parseInt(e.target.value))}></input>
+        <button>Add</button>
+      </form>
     </div>
   )
 }
